c30a79ea7714513cd09829cabc6e50e126ed6d39
Igor Srajer <igorsrajer123@gmail.com>
2024-02-24 16:18:35 +0100

    Setup typescript, eslint and initial project configuration

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import { Request, Response } from "express";
+
 const express = require("express");
 const dotenv = require("dotenv");
 
@@ -6,12 +8,11 @@ dotenv.config();
 const app = express();
 const port = process.env.LOCALHOST_SERVER_PORT;
 
-app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Goca madjija voli bureka a voli i pojesti slatko!!!!");
 });
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost: ${port}`);
-  console.log(`[process]: ${process}`);
-  console.log(`[process]: ${process}`);
+  console.log(process.env);
 });
